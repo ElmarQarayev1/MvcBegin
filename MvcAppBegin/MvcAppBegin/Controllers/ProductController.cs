bbbffd8f5e198ae3583c9c2dda8484e4cb9d6455
ElmarQarayev1 <elmareg@code.edu.az>
2024-04-16 11:17:36 +0400

    deyisiklik edildi

--- MvcAppBegin/MvcAppBegin/Controllers/ProductController.cs
+++ MvcAppBegin/MvcAppBegin/Controllers/ProductController.cs
@@ -7,23 +7,23 @@ namespace MvcAppBegin.Controllers
 {
 	public class ProductController:Controller
 	{
-		private List<Product>? _products;
+        private List<Product>? _products;
 
-		public ProductController()
-		{
-			_products = new List<Product>()
-			{
-				new Product{Id=1,Name="samsung",Price=87},
-                new Product{Id=2,Name="iphone",Price=103},
-                new Product{Id=3,Name="nokia",Price=45},
-                new Product{Id=4,Name="honor",Price=413},
-               new Product{Id=5,Name="redmi",Price=123},
-            };
-		}
-		public JsonResult GetAll()
-		{
+        public ProductController()
+        {
+            _products = new List<Product>()
+            {
+                new Product{Id=1,Name="samsung",Price=87},
+                      new Product{Id=2,Name="iphone",Price=103},
+                      new Product{Id=3,Name="nokia",Price=45},
+                      new Product{Id=4,Name="honor",Price=413},
+                     new Product{Id=5,Name="redmi",Price=123},
+                  };
+        }
+        public JsonResult GetAll()
+        {
             return Json(_products);
-		}
+        }
         public ViewResult Index()
         {
             ViewBag.Products = _products;
@@ -32,15 +32,11 @@ namespace MvcAppBegin.Controllers
         }
         public ViewResult Details(int id)
         {
-            Product product = _products.FirstOrDefault(x => x.Id == id);
-
-            if (product == null)
-            {
-                return View("Error");
-            }
+            Product? product = _products?.FirstOrDefault(x => x.Id == id);
             ViewBag.Product = product;
             return View();
         }
+
     }
 }
 
